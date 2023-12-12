e5d3c6a9a7e1b47cdb7814d0e6d7c2f5820ef286
Andrei Belov <abelov@student.42london.com>
2023-12-12 19:29:40 +0000

    get rid of global_var

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -12,25 +12,14 @@
 
 #include "ft_printf.h"
 
-static t_ft_print_dispatch_f	const
-	g_ft_print_dispatch[UCHAR_MAX] = {
-['c'] = ft_print_c,
-['%'] = ft_print_percent,
-['d'] = ft_print_d,
-['p'] = ft_print_p,
-['i'] = ft_print_d,
-['u'] = ft_print_u,
-['x'] = ft_print_x,
-['X'] = ft_print_upperx,
-['s'] = ft_print_s,
-};
-
 void	ft_putchar(char c)
 {
 	ft_putchar_fd(c, STDOUT_FILENO);
 }
 
-int	do_print(const char	*fmt, va_list *argp, void (*putc)(char))
+int	do_print(const char	*fmt, va_list *argp,
+				void (*putc)(char),
+				t_ft_print_dispatch_f const	ft_print_dispatch[])
 {
 	size_t					i;
 	register char			c;
@@ -47,8 +36,8 @@ int	do_print(const char	*fmt, va_list *argp, void (*putc)(char))
 			continue ;
 		}
 		c = *++fmt;
-		if (c != '\0' && g_ft_print_dispatch[(unsigned char) c])
-			i += g_ft_print_dispatch[(unsigned char) c](argp);
+		if (c != '\0' && ft_print_dispatch[(unsigned char) c])
+			i += ft_print_dispatch[(unsigned char) c](argp);
 		else
 			fmt--;
 		c = *++fmt;
@@ -58,9 +47,20 @@ int	do_print(const char	*fmt, va_list *argp, void (*putc)(char))
 
 int	ft_printf(const char *format, ...)
 {
-	va_list		args;
-	char		*str;
-	int			count;
+	va_list								args;
+	char								*str;
+	int									count;
+	static t_ft_print_dispatch_f const	ft_print_dispatch[UCHAR_MAX] = {
+	['c'] = ft_print_c,
+	['%'] = ft_print_percent,
+	['d'] = ft_print_d,
+	['p'] = ft_print_p,
+	['i'] = ft_print_d,
+	['u'] = ft_print_u,
+	['x'] = ft_print_x,
+	['X'] = ft_print_upperx,
+	['s'] = ft_print_s,
+	};
 
 	if (!format || !*format)
 		return (0);
@@ -68,7 +68,7 @@ int	ft_printf(const char *format, ...)
 	if (!str || *str == '\0')
 		return (0);
 	va_start(args, format);
-	count = do_print(str, &args, ft_putchar);
+	count = do_print(str, &args, ft_putchar, ft_print_dispatch);
 	va_end(args);
 	free(str);
 	return (count);
